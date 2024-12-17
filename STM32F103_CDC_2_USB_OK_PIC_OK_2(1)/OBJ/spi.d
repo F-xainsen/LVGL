6977ceb6faec5a99ad1787e33d2db2af595032bb
F-xainsen <2581463919@qq.com>
2024-12-17 17:18:28 +0800

    可以接受图片

--- STM32F103_CDC_2_USB_OK_PIC_OK_2(1)/OBJ/spi.d
+++
@@ -1,2 +0,0 @@
-..\obj\spi.o: ..\HARDWARE\ST7789\spi.c
-..\obj\spi.o: ..\HARDWARE\ST7789\spi.h
--- STM32F103_CDC_2_USB_OK_PIC_OK_2(1)/OBJ/st7789.d
+++
@@ -1,2 +0,0 @@
-..\obj\st7789.o: ..\HARDWARE\ST7789\st7789.c
-..\obj\st7789.o: ..\HARDWARE\ST7789\st7789.h
